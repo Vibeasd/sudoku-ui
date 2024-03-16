21983db618bc1903cbf9435563b1d633e07f9b21
Balázs Bence <balazsbence.sas@gmail.com>
2024-03-16 17:55:20 +0100

    feat: create menu ui

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { BoardContainerComponent } from './board-container/board-container.component';
+import { MenuComponent } from './menu/menu.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: 'board', component: BoardContainerComponent }, //TODO route guard here, only with chosen set up
+	{ path: '**', component: MenuComponent },
+];
---
+++ src/app/board-container/board-container.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-board-container',
+  standalone: true,
+  imports: [],
+  templateUrl: './board-container.component.html',
+  styleUrl: './board-container.component.scss'
+})
+export class BoardContainerComponent {
+
+}
