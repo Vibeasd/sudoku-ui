3179fa254ff8c39464b57803eae0b37ae52e05b2
Balázs Bence <balazsbence.sas@gmail.com>
2024-03-16 20:06:14 +0100

    fix: prettifying

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-menu',
-  standalone: true,
-  imports: [],
-  templateUrl: './menu.component.html',
-  styleUrl: './menu.component.scss'
+	selector: 'app-menu',
+	standalone: true,
+	imports: [],
+	templateUrl: './menu.component.html',
+	styleUrl: './menu.component.scss',
 })
-export class MenuComponent {
-
-}
+export class MenuComponent {}
