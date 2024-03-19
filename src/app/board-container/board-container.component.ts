914da10ece03b2089efbf0a80ddc37983492bbb6
Balázs Bence <balazsbence.sas@gmail.com>
2024-03-19 15:03:17 +0100

    docs: create jsdocs

--- src/app/board-container/board-container.component.ts
+++ src/app/board-container/board-container.component.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @ngrx/no-typed-global-store */
 import { CommonModule, NgClass } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
@@ -15,6 +16,9 @@ import {
 	selectIsValid,
 } from '../sudoku-sdk/sudoku.selector';
 
+/**
+ * Container for sudoku game board. This is a smart component which handles the game events.
+ */
 @Component({
 	selector: 'app-board-container',
 	standalone: true,
@@ -24,41 +28,85 @@ import {
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BoardContainerComponent {
+	/**
+	 * Contains the current game difficulty setting.
+	 */
 	readonly difficulty$: Observable<Difficulty | undefined> = this.store.select(selectDifficulty);
+
+	/**
+	 * It contains the game mode setting.
+	 */
 	readonly isSingle$: Observable<boolean> = this.store
 		.select(selectGameMode)
 		.pipe(map((gameMode) => gameMode === 'single'));
+
+	/**
+	 * It holds every information about the player one's game board.
+	 */
 	readonly boardOne$: Observable<Board> = this.store.select(selectBoardOneInitial);
+
+	/**
+	 * It holds every information about the player two's game board.
+	 */
 	readonly boardTwo$: Observable<Board> = this.store.select(selectBoardTwoInitial);
+
+	/**
+	 * If there was a sudoku validation, this prop holds the result of that validation.
+	 */
 	readonly isValid$: Observable<boolean> = this.store.select(selectIsValid);
+
+	/**
+	 * If a sudoku board was solved this prop holds about that information.
+	 */
 	readonly isSolved$: Observable<boolean> = this.store.select(selectIsSolved);
 
 	constructor(private store: Store<AppState>) {}
 
+	/**
+	 * This method triggers the solving of the player one's board at its current state.
+	 */
 	solveBoardOne() {
 		this.store.dispatch(sudokuActions.solveBoardOne());
 	}
 
+	/**
+	 * This method triggers the solving of the player two's board at its current state.
+	 */
 	solveBoardTwo() {
 		this.store.dispatch(sudokuActions.solveBoardTwo());
 	}
 
-	solveValidateOne() {
+	/**
+	 * This method triggers validation on the player one's board at its current state.
+	 */
+	validateBoardOne() {
 		this.store.dispatch(sudokuActions.validateBoardOne());
 	}
 
-	solveValidateTwo() {
+	/**
+	 * This method triggers validation on the player two's board at its current state.
+	 */
+	validateBoardTwo() {
 		this.store.dispatch(sudokuActions.validateBoardTwo());
 	}
 
+	/**
+	 * It triggers a popup which contains information about the board solving was successful or not.
+	 */
 	solvedConfirmed() {
 		this.store.dispatch(sudokuActions.solvingConfirmed());
 	}
 
+	/**
+	 * It triggers a popup which contains information about the board was valid or not.
+	 */
 	validityConfirmed() {
 		this.store.dispatch(sudokuActions.validityConfirmed());
 	}
 
+	/**
+	 * If the game is not yet solved then we can start it from the initial state by calling this method.
+	 */
 	restartGame() {
 		this.store.dispatch(sudokuActions.restartGame());
 	}
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -5,6 +5,9 @@ import { Store } from '@ngrx/store';
 import { Difficulty } from '../model/sudoku.types';
 import { sudokuActions } from '../sudoku-sdk/sudoku.action';
 
+/**
+ * It contains a menu form where the user is able to select his/her game settings and start playing.
+ */
 @Component({
 	selector: 'app-menu',
 	standalone: true,
@@ -16,13 +19,22 @@ import { sudokuActions } from '../sudoku-sdk/sudoku.action';
 export class MenuComponent {
 	private store: Store = inject(Store);
 
+	/**
+	 * It handles menu form values
+	 */
 	menuForm: FormGroup = new FormGroup({
 		gameMode: new FormControl('single', Validators.required),
 		gameDifficulty: new FormControl('easy', Validators.required),
 	});
 
+	/**
+	 * Difficulty options for the sudoku
+	 */
 	difficulties: Difficulty[] = ['easy', 'medium', 'hard', 'random'];
 
+	/**
+	 * It submits the menu form and starts the game for the user. It also populate our initial state for the game.
+	 */
 	onSubmit() {
 		this.store.dispatch(
 			sudokuActions.submitGameDetails({
